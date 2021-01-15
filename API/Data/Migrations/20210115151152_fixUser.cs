b3b2357fcb62235b616d232e211b4618ffe02f1f
Ron Milich <ronmilich123@gmail.com>
2021-01-15 17:30:53 +0200

    Extended Api

---
+++ API/Data/Migrations/20210115151152_fixUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class fixUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsMail",
+                table: "Photos",
+                newName: "IsMain");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsMain",
+                table: "Photos",
+                newName: "IsMail");
+        }
+    }
+}
